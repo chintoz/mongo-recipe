6edf9d372bbc4517b73fc717ef601538b4c61c0b
Jacinto J. Mena Lomeña <jacintomena@gmail.com>
2021-01-23 01:03:41 +0100

    Changes to add entities Recipe and Notes.

---
+++ src/main/java/es/menasoft/recipe/domain/Notes.java
@@ -0,0 +1,35 @@
+package es.menasoft.recipe.domain;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
