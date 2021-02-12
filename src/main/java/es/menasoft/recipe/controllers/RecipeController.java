dc262e1a5046557f4e81a5645362f257b72c9b20
Jacinto J. Mena Lomeña <jacintomena@gmail.com>
2021-02-13 00:42:00 +0100

    Changes to add recipe creation UI.

--- src/main/java/es/menasoft/recipe/controllers/RecipeController.java
+++ src/main/java/es/menasoft/recipe/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package es.menasoft.recipe.controllers;
 
+import es.menasoft.recipe.commands.RecipeCommand;
 import es.menasoft.recipe.service.RecipeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +20,19 @@ public class RecipeController {
     public String showRecipe(@PathVariable Long id, Model model) {
         model.addAttribute("recipe", recipeService.findById(id));
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand result = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + result.getId();
     }
 }
