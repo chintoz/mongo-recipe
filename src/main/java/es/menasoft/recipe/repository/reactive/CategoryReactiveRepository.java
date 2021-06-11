cb3628a129dd04c145ef198f9b3a54c2ae28ee83
Jacinto J. Mena Lomeña <jacintomena@gmail.com>
2021-06-12 00:49:15 +0200

    Changes to add a couple of operations in reactive repositories.

--- src/main/java/es/menasoft/recipe/repository/reactive/CategoryReactiveRepository.java
+++ src/main/java/es/menasoft/recipe/repository/reactive/CategoryReactiveRepository.java
@@ -2,6 +2,9 @@ package es.menasoft.recipe.repository.reactive;
 
 import es.menasoft.recipe.domain.Category;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface CategoryReactiveRepository extends ReactiveMongoRepository<Category, String> {
+
+    Mono<Category> findByDescription(String description);
 }
--- src/main/java/es/menasoft/recipe/repository/reactive/UnitOfMeasureReactiveRepository.java
+++ src/main/java/es/menasoft/recipe/repository/reactive/UnitOfMeasureReactiveRepository.java
@@ -2,6 +2,9 @@ package es.menasoft.recipe.repository.reactive;
 
 import es.menasoft.recipe.domain.UnitOfMeasure;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
 public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+
+    Mono<UnitOfMeasure> findByDescription(String description);
 }
