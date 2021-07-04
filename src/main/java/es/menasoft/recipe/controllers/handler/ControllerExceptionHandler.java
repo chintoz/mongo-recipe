a5e6fac06def80226d912eabacc1c72c9c82fa7e
Jacinto J. Mena Lomeña <jacintomena@gmail.com>
2021-07-05 00:43:16 +0200

    Changes to manage binding exception at controller level using a ControllerAdvice.

--- src/main/java/es/menasoft/recipe/controllers/handler/ControllerExceptionHandler.java
+++ src/main/java/es/menasoft/recipe/controllers/handler/ControllerExceptionHandler.java
@@ -1,24 +1,24 @@
 package es.menasoft.recipe.controllers.handler;
 
 import lombok.extern.slf4j.Slf4j;
-//import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.ResponseStatus;
-// import org.springframework.web.servlet.ModelAndView;
-
-//import java.util.Map;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    //@ResponseStatus(HttpStatus.BAD_REQUEST)
-    //@ExceptionHandler(NumberFormatException.class)
-    //public ModelAndView handleNumberFormat(NumberFormatException exception) {
-    //    log.error("Handled number format exception");
-    //    log.error(exception.getMessage());
-    //    return new ModelAndView("400error", Map.of("exception", exception));
-    //}
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormat(WebExchangeBindException exception, Model model) {
+        log.error("Handled binding exception");
+        log.error(exception.getMessage());
+        model.addAttribute("exception", exception);
+        return "400error";
+    }
 
 }
